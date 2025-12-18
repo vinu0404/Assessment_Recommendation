6424695938d64373a64f81ec2b9782314563b341
vinu0404 <22cd3034@rgipt.ac.in>
2025-12-19 00:24:22 +0530

    fix error

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -2,7 +2,7 @@ from app.utils.logger import get_logger, app_logger
 from app.utils.validators import validate_url, validate_query_length, extract_urls_from_text
 from app.utils.formatters import format_assessment_response,extract_json_from_response
 from app.utils.helpers import  clean_text,chunk_list,extract_duration_from_text
-from assessment_map import get_assessment_map,get_fallback_skill
+from app.utils.assessment_map import get_assessment_map,get_fallback_skill
 
 __all__ = [
     "get_logger",
